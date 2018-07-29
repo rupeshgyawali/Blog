6799409a3be12468e5005eb357773bb0f933bf98
rupeshgyawali <rupeshgyawali49@gmail.com>
2018-07-29 16:30:25 +0545

    login, register, user profile and follow unfollow

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, UserProfile
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(UserProfile)
 
